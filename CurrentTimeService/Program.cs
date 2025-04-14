5fb00b4cdc651f5abfcb03c2ba961fdc3dc38a28
Andres Guerrero <aguerrero@estropical.com>
2025-04-14 16:44:59 -0400

    Added a simple post

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -12,5 +12,6 @@ app.UseHttpsRedirection();
 
 // GET UTC
 app.MapGet("time/utc", () => Results.Ok("This is the current UTC time: " + DateTime.UtcNow));
+app.MapPost("This is the POST", () => Results.Ok("This is what happens when OK 200"));
 
 await app.RunAsync();
